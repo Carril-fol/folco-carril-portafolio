ff2aa61742281167cf645c1db29a607b0810c416
Folco Carril <folco.carril@gmail.com>
2026-07-22 21:06:26 -0300

    chore: Updated color of icons and text

--- src/app/components/personal/ContactInformation.tsx
+++ src/app/components/personal/ContactInformation.tsx
@@ -8,8 +8,8 @@ interface ContactInformationProps {
 export const ContactInformation: React.FC<ContactInformationProps> = ({ text, icon: Icon }) => {
   return (
     <div className="flex items-center gap-2 mt-1">
-      <Icon className="text-gray-600 w-[15] h-[15]" />
-      <span className="text-sm font-bold text-gray-500">{text}</span>
+      <Icon className="text-[#94a3b8] w-4 h-4" />
+      <span className="text-sm font-medium text-[#94a3b8]">{text}</span>
     </div>
   );
 };
